f5aa32f0e5357082e49c95a90e9a84b636e2eb56
Stephen <steveyoung359@gmail.com>
2014-09-15 00:23:39 -0500

    sun moves in circular motion and imported jquery

--- public/js/controllers/frame/default/default.js
+++ public/js/controllers/frame/default/default.js
@@ -16,7 +16,32 @@ angular.module('personal.default', [
     $locationProvider.html5Mode(true);
 
   })
-  .controller('defaultCtrl', function($scope, $location){ 
+  .controller('defaultCtrl', function($scope, $location){
+    var newLeft = -200;
+    var newBottom = -100;
+    var returning = null;
 
+    console.log(window.innerHeight);
+
+    var t = 0;
+
+    function moveit() {
+        t += 0.05;
+        var r = window.innerWidth/2;
+        var xcenter = window.innerWidth/2;
+        var ycenter = window.innerWidth/2;
+        var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
+        var newTop = Math.floor(ycenter + (r * Math.sin(t)));
+        $('#sunbox').animate({
+            top: newTop,
+            left: newLeft,
+        }, 100, function() {
+            moveit();
+        });
+    }
+
+    $(document).ready(function() {
+      moveit();
+    });
 
   });
