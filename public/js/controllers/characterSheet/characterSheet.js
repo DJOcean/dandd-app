b3f7428a568202858be7adfafd88d4aa7e1d6923
DJOcean <david.youngjames@gmail.com>
2014-09-23 18:48:16 -0500

    finished skills

--- public/js/controllers/characterSheet/characterSheet.js
+++ public/js/controllers/characterSheet/characterSheet.js
@@ -75,7 +75,148 @@ angular.module('dnd.characterSheet', [
         },
         animalEmpathy: {
           rank: 0
-        }
+        },
+        appraise: {
+          rank: 0
+        },
+        balance: {
+          rank: 0
+        },
+        bluff: {
+          rank: 0
+        },
+        climb: {
+          rank: 0
+        },
+        concentration: {
+          rank: 0
+        },
+        craft: {
+          rank: 0
+        },
+        decipherScript: {
+          rank: 0
+        },
+        diplomacy: {
+          rank: 0
+        },
+        disableDevice: {
+          rank: 0
+        },
+        disguise: {
+          rank: 0
+        },
+        escapeArtist: {
+          rank: 0
+        },
+        forgery: {
+          rank: 0
+        },
+        gatherInformation: {
+          rank: 0
+        },
+        handleAnimal: {
+          rank: 0
+        },
+        heal: {
+          rank: 0
+        },
+        hide: {
+          rank: 0
+        },
+        innuendo: {
+          rank: 0
+        },
+        intimidate: {
+          rank: 0
+        },
+        intuitDirection: {
+          rank: 0
+        },
+        jump: {
+          rank: 0
+        },
+        knowledgeArcana: {
+          rank: 0
+        },
+        knowledgeEngineering: {
+          rank: 0
+        },
+        knowledgeGeography: {
+          rank: 0
+        },
+        knowledgeHistory: {
+          rank: 0
+        },
+        knowledgeLocal: {
+          rank: 0
+        },
+        knowledgeNature: {
+          rank: 0
+        },
+        knowledgeNobility: {
+          rank: 0
+        },
+        knowledgePlanes: {
+          rank: 0
+        },
+        knowledgeReligion: {
+          rank: 0
+        },
+        listen: {
+          rank: 0
+        },
+        moveSilenty: {
+          rank: 0
+        },
+        openLock: {
+          rank: 0
+        },
+        perform: {
+          rank: 0
+        },
+        pickPocket: {
+          rank: 0
+        },
+        profession: {
+          rank: 0
+        },
+        readLips: {
+          rank: 0
+        },
+        ride: {
+          rank: 0
+        },
+        scry: {
+          rank: 0
+        },
+        search: {
+          rank: 0
+        },
+        senseMotive: {
+          rank: 0
+        },
+        spellcraft: {
+          rank: 0
+        },
+        spot: {
+          rank: 0
+        },
+        swim: {
+          rank: 0
+        },
+        tumble: {
+          rank: 0
+        },
+        useMagicDevice: {
+          rank: 0
+        },
+        useRope: {
+          rank: 0
+        },
+        wildernessLore: {
+          rank: 0
+        },
       },
       gear: [
         {
