a74f5d1b12ac4ed25c9513ba2743a6f52e75fb1e
DJOcean <david.youngjames@gmail.com>
2014-09-23 17:13:25 -0500

    major changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,24 @@
 
 // gulp plugins
 var gulp       = require('gulp'),
-  scsslint     = require('gulp-scss-lint')
+  scsslint     = require('gulp-scss-lint'),
+  sass         = require('gulp-sass')
 
 var exitOnError = !!gulp.env['exit-on-error'];
 
 gulp.task('default', function() {
-  gulp.src('./public/styles/**/*.scss')
-    .pipe(scsslint())
+ gulp.run('lint-scss', 'styles');
 });
 
 gulp.task('lint-scss', function() {
   gulp.src('./public/styles/**/*.scss')
     .pipe(scsslint())
-
 });
+
+gulp.task('styles', function() {
+	gulp.src('./public/styles/**/*.scss')
+   		.pipe(sass())
+   		.pipe(gulp.dest('./public/styles'));
+});
+
 
